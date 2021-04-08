b96d2c697a5bb01baf08e8e57ee449e6832431c7
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-04-08 10:44:58 -0500

    app fixed

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,11 +1,38 @@
-const app = (() =>{
-    const mapeo = (weather) =>{
-        console.log(weather.data)
-    }
-    const getWeatherByCity = (city) =>{
-        apiclient.getWeatherByCity(city,mapeo);
-    }
-    return{
-        getWeatherByCity:getWeatherByCity
-    }
+const app = (() => {
+	const mapeo = (weather) => {
+		let table1 = $("#clima > tbody");
+		let table0 = $("#cityinfo > tbody");
+		let wd = weather.data;
+		table0.empty();
+		table1.empty();
+		table0.append(
+			`<tr> 
+                      <td>${wd.name}</td>
+                      <td>${wd.timezone}</td>
+					  <td>${wd.coord.long}</td>
+					  <td>${wd.coord.lat}</td>
+                      <td>${wd.visibility}</td>
+					  <td>${wd.clouds.all}</td>
+					  <td>${wd.cod}</td>
+			          <td>Wind speed</td><td>${wd.wind.speed}</td>
+                	 <tr><td>Wind deg</td><td>${wd.wind.deg}</td></tr>
+			</tr>`
+		);
+		table1.append(
+			`<tr> 
+					  <td>${wd.weather.id}</td>
+                      <td>${wd.weather.main}</td>
+                      <td>${wd.weather.description}</td>
+					 
+                </tr>`
+		);
+
+
+	}
+	const getWeatherByCity = (city) => {
+		apiclient.getWeatherByCity(city, mapeo);
+	}
+	return {
+		getWeatherByCity: getWeatherByCity
+	}
 })();
